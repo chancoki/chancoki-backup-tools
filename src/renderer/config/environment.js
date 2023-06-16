b22bea1732801a187418e9e4ecc8892eb1af2ae4
chancoki <chhj727@163.com>
2023-06-16 17:46:22 +0800

    fest: 登录注册

--- src/renderer/config/environment.js
+++ src/renderer/config/environment.js
@@ -1,1 +1,1 @@
-export const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "http://192.168.10.53:10030";
+export const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://code.funfly.fun";
